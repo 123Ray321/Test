3b36a1160d33e26d3013b73aba891b01ac6e6299
ray <326523853.com>
2019-11-20 21:47:28 +0800

    HelloWorld(Ray)

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+	std::count << "Hello World1!";
+	std::count << "Hello World2!";
+	std::count << "Hello World3!";
+	std::count << "Hello World4!";
+	std::count << "Hello World5!";
+	std::count << "Hello World6!";
+	return 0;
+}

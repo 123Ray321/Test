e02e5ffdebb7e85fde2eb633ccdbef327dced811
ray <326523853.com>
2019-11-21 15:45:36 +0800

    other

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main()
+{
+	std::cout << "Hello World1!";
+	std::cout << "Hello World2!";
+	std::cout << "Hello World3!";
+	std::cout << "Hello World4!";
+	std::cout << "Hello World5!";
+	std::cout << "Hello World6!";
+	std::cout << "Other";
+	return 0;
+}
